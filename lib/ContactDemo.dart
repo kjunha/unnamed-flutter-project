0daf5c8b00d6a34cb15c24b1e1295d1f33101f56
kjunha <kjunha77@gmail.com>
2020-06-07 13:30:40 +0900

    shift

---
+++ lib/ContactDemo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'ContactListPage.dart';
+import 'ContactDetailPage.dart';
+
+void main() {
+  runApp(ContactDemo());
+}
+
+
+
+class ContactDemo extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Form',
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
+
